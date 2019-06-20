e43df360e3f6e186e3b3437cadb6e3faeb84c278
OvoNely <lisper@mai.ru>
2019-06-20 20:20:44 +0700

    ++ Transport

---
+++ src/Transport.java
@@ -0,0 +1,13 @@
+public class Transport {
+
+    public String name;
+    public int maxSpeed;
+    public int passengersNumber;
+
+    public Transport(String name, int maxSpeed, int passengerNumber) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.passengersNumber = passengerNumber;
+    }
+
+}
